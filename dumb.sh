c84de427b78b470e636ca0aeb23682b1524ead28
Dimitar Dimitrov <dmtrdmtrov@gmail.com>
2021-03-19 08:58:55 +0200

    Fixup script
    * Fix permissions
    * Remove unnecessary set
    * Set trap to remove all temporary files even on error
    * Replace `cp` + `rm` with `mv`
    * Make `wget` quiet
    * Remove unnecessary `!` before `aplay`

--- dumb.sh
+++ dumb.sh
@@ -3,20 +3,19 @@
 script_dir="$(dirname "$(realpath "$0")")"
 source "$script_dir/lib/statuslib/statuslib.sh"
 
-set -eEuo pipefail
+trap 'rm -f random.txt suicidenote.txt free_sex.wav 2>/dev/null' EXIT
 
 step "Creating a random file..."
 echo 'oieojejqJET;QEYH;;OEOJHQHJGJ' > random.txt
 
 substep "Renaming the random file to something even more random..."
-cp random.txt suicidenote.txt
-rm random.txt
+mv random.txt suicidenote.txt
 step "Wasting more of your time..."
 sleep 3
 step "Downloading Free Sex 2022 Safe Punjabi No Virus..."
-wget https://cdn.discordapp.com/attachments/821062463810175046/822351991539367956/free_sex.wav
+wget -q https://cdn.discordapp.com/attachments/821062463810175046/822351991539367956/free_sex.wav
 substep "Trolling you..."
-! aplay free_sex.wav
+aplay free_sex.wav
 subinfo "You have just been trolled"
 sleep 0.5
 step "Downloading child porn..."
@@ -26,5 +25,3 @@ sleep 3
 step "Have fun with the swat team incoming!"
 echo
 info "Credit to DimiDimit for creating statuslib, you shouldn't have sent this to me on Discord..."
-rm suicidenote.txt
-rm free_sex.wav
